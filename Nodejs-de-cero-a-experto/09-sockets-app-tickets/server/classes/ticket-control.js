76ee6a5bf2ae26836151fd3a6e6acf2f6a94d428
Nilson Nieto López <nilson-sn@outlook.com>
2018-10-22 09:01:02 -0500

    App OK

--- Nodejs-de-cero-a-experto/09-sockets-app-tickets/server/classes/ticket-control.js
+++ Nodejs-de-cero-a-experto/09-sockets-app-tickets/server/classes/ticket-control.js
@@ -1,127 +1,100 @@
 const fs = require("fs");
 
-
-class Ticket{
-
-    constructor(numero,escritorio){
-
-        this.numero=numero;
-        this.escritorio=escritorio;
-    }
-
+class Ticket {
+  constructor(numero, escritorio) {
+    this.numero = numero;
+    this.escritorio = escritorio;
+  }
 }
 
-class TicketControl{
+class TicketControl {
+  constructor() {
+    this.ultimo = 4;
+    this.dia = new Date().getDate();
+    this.tickets_pendientes = [];
+    this.tickets_en_atencion = [];
 
-    constructor(){
-
-        this.ultimo = 4;
-        this.dia = new Date().getDate();
-        this.tickets_pendientes=[];
-        this.tickets_en_atencion=[];
-        this.numero_escritorio_busqueda;
-        
-        let data = require("../data/data.json");
-        if(data.dia===this.dia){
-            
-            this.ultimo=data.ultimo;
-            this.tickets_pendientes= data.tickets_pendientes;
-            this.tickets_en_atencion=data.tickets_en_atencion;
-        
-        }else{
-            this.reiniciarConteno();
-        }
+    let data = require("../data/data.json");
+    if (data.dia === this.dia) {
+      this.ultimo = data.ultimo;
+      this.tickets_pendientes = data.tickets_pendientes;
+      this.tickets_en_atencion = data.tickets_en_atencion;
+    } else {
+      this.reiniciarConteno();
     }
-     reiniciarConteno() {
-        this.ultimo=0;
-        this.tickets_pendientes=[];
-        this.tickets_en_atencion=[];
+  }
+  reiniciarConteno() {
+    this.ultimo = 0;
+    this.tickets_pendientes = [];
+    this.tickets_en_atencion = [];
 
-        this.salvarCambios();
-        console.log("Se ha inicializado el sistema");
-    }
+    this.salvarCambios();
+    console.log("Se ha inicializado el sistema");
+  }
 
+  siguienteTicket() {
+    ++this.ultimo;
+    let nuevo_ticket = new Ticket(this.ultimo, null);
+    this.tickets_pendientes.push(nuevo_ticket);
 
-    siguienteTicket(){
-        ++this.ultimo;
-        let nuevo_ticket = new Ticket(this.ultimo,null);
-        this.tickets_pendientes.push(nuevo_ticket);
+    this.salvarCambios();
+    return this.ultimo;
+  }
 
-        this.salvarCambios();
-        return this.ultimo;
+  getEstadoTicket() {
+    return this.ultimo;
+  }
 
-    }
+  getTicketsEnAtencion() {
+    return this.tickets_en_atencion;
+  }
 
-    getEstadoTicket(){
-        return this.ultimo;
-    }
-
-
-    getTicketsEnAtencion(){
-
-        return this.tickets_en_atencion;
-
-    }
-  
-
-    asignarTicket(escritorio){
-
-        if( this.tickets_pendientes.length===0){
-
-            return {
-                ok:false,
-                message:'No hay tickets pendientes'
-            }
-        }
-
-        let numero_ticket = this.tickets_pendientes[0].numero;
-        this.tickets_pendientes.shift();
-
-        let nuevo_ticket= new Ticket(numero_ticket,escritorio);
-        console.log('En atencion',this.tickets_en_atencion);
-
-        function  findByEscritorio(element) {
-            console.log('Escritorio',escritorio);
-            return element.escritorio===escritorio;
-        }
-        // A lo mal, buscaré otra solucion
-          this.numero_escritorio_busqueda=escritorio;
-
-        var index_escritorio = this.tickets_en_atencion.findIndex(findByEscritorio);
-
-     this.tickets_en_atencion.splice(index_escritorio,index_escritorio,nuevo_ticket);
-
-        console.log('En atencion despues',this.tickets_en_atencion);
-
-        if(this.tickets_en_atencion.length>4){
-            this.tickets_en_atencion.splice(-1,1);
-        }
-        this.salvarCambios();
-
-        return nuevo_ticket;
-
+  asignarTicket(escritorio) {
+    if (this.tickets_pendientes.length === 0) {
+      return {
+        ok: false,
+        message: "No hay tickets pendientes"
+      };
     }
 
-     
+    let numero_ticket = this.tickets_pendientes[0].numero;
+    this.tickets_pendientes.shift();
 
-    salvarCambios(){
-        let Data={
-            ultimo: this.ultimo,
-            dia : this.dia,
-            tickets_pendientes:this.tickets_pendientes,
-            tickets_en_atencion:this.tickets_en_atencion
-        } 
-        
-        let jsonData = JSON.stringify(Data);
-   
-   
-        fs.writeFileSync("./server/data/data.json",jsonData);
+    let nuevo_ticket = new Ticket(numero_ticket, escritorio);
 
-   
+    var index_escritorio = this.tickets_en_atencion.findIndex(
+      element => element.escritorio
+    );
+
+    if (index_escritorio !== -1) {
+      this.tickets_en_atencion[index_escritorio] = nuevo_ticket;
+    } else {
+      this.tickets_en_atencion.push(nuevo_ticket);
     }
+
+
+    if (this.tickets_en_atencion.length > 4) {
+      this.tickets_en_atencion.splice(-1, 1);
+    }
+    this.salvarCambios();
+
+    return nuevo_ticket;
+  }
+
+  salvarCambios() {
+    let Data = {
+      ultimo: this.ultimo,
+      dia: this.dia,
+      tickets_pendientes: this.tickets_pendientes,
+      tickets_en_atencion: this.tickets_en_atencion
+    };
+
+    let jsonData = JSON.stringify(Data);
+
+    fs.writeFileSync("./server/data/data.json", jsonData);
+  }
 }
 
-
-module.exports={
-    TicketControl
-}
+module.exports = {
+  TicketControl
+};
