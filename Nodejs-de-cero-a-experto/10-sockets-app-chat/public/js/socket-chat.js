7239532d3e834e8cc88769b0ee71cae8fb1d6243
NSnietol <nilson-sn@outlook.com>
2018-10-22 21:56:14 -0500

    Agregando y eliminando usuarios, notificando cambios

--- Nodejs-de-cero-a-experto/10-sockets-app-chat/public/js/socket-chat.js
+++ Nodejs-de-cero-a-experto/10-sockets-app-chat/public/js/socket-chat.js
@@ -1,15 +1,35 @@
 var socket = io();
 
+var searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('usuario')) {
+
+    window.location = 'index.html';
+    throw new Error("El usuario es necesario");
+
+
+}
+
+console.log(searchParams.get('usuario'));
+let usuario = searchParams.get('usuario');
+
 socket.on("connect", function() {
-    
+
     console.log("Conectado nuevo chat");
-    socket.emit('ingresarChat',
-    
-    {usuario:'Nilson'});
+    socket.emit('ingresarChat', { usuario },
+        function(resp) {
+
+            console.log("Lista de usuarios", resp);
+        });
+    socket.on('clientesActivos', (data) => {
+
+        console.log('Usuarios activos', data);
+    })
+
+
 
 });
-  
-  socket.on("disconnect", function() {
+
+socket.on("disconnect", function() {
     console.log("Desconectado  chat");
-  });
-  
+});
--- Nodejs-de-cero-a-experto/10-sockets-app-chat/server/sockets/socket.js
+++ Nodejs-de-cero-a-experto/10-sockets-app-chat/server/sockets/socket.js
@@ -1,12 +1,33 @@
-const {io} = require("../server");
+const { io } = require("../server");
 
 
-io.on('connection',(cliente)=>{
+const { Usuarios } = require("../classes/usuario.js");
+
+let usuarios = new Usuarios();
+
+
+io.on('connection', (cliente) => {
 
     console.log('Nuevo usuario');
-    cliente.on('ingresarChat',(data)=>{
+
+    cliente.on('ingresarChat', (data, callback) => {
         console.log(data);
+        if (!data.usuario) {
+            return callback({
+                ok: false,
+                message: 'El nombre es requerido'
+            })
+
+        }
+        let personas = usuarios.agregarPersona(cliente.id, data.usuario);
+        cliente.broadcast.emit('clientesActivos', personas);
+
+        return callback(personas);
     });
-    
+
+    cliente.on('disconnect', () => {
+        usuarios.borrarPersona(cliente.id);
+        cliente.broadcast.emit('clientesActivos', usuarios.getPersonas());
+    });
 
 });
